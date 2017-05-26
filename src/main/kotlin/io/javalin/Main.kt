1882daf239ea16219e90706bc32daacd6c2f7562
David <davidaase@hotmail.com>
2017-05-26 16:33:32 +0200

    Add heroku deployment

--- src/main/kotlin/io/javalin/Main.kt
+++ src/main/kotlin/io/javalin/Main.kt
@@ -9,7 +9,7 @@ fun main(args: Array<String>) {
     var todos = arrayOf(Todo(123123123, "My very first todo", false))
 
     val app = Javalin.create()
-            .port(7000)
+            .port(getHerokuAssignedPort())
             .enableStaticFiles("/public")
 
     app.routes {
@@ -22,4 +22,12 @@ fun main(args: Array<String>) {
         }
     }
 
+}
+
+private fun getHerokuAssignedPort(): Int {
+    val processBuilder = ProcessBuilder()
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"))
+    }
+    return 7000
 }
