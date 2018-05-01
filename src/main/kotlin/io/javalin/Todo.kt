93a46242ba60dac4f46e4944ef374d3a563f1776
David <davidaase@hotmail.com>
2018-05-01 13:06:11 +0200

    bump

--- src/main/kotlin/io/javalin/Todo.kt
+++ src/main/kotlin/io/javalin/Todo.kt
@@ -1,3 +1,3 @@
 package io.javalin
 
-data class Todo(val id: Long = -1, val title: String = "", val completed: Boolean = false)
+data class Todo(val id: Long, val title: String, val completed: Boolean)
